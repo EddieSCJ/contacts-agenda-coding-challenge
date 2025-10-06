95c2589771cb26afc47a79732de6528f67d360e5
eddiescj <eddieprofessionalmail@gmail.com>
2025-10-05 23:52:12 -0300

    Add main application class and test class for Contacts Agenda application

---
+++ src/main/java/com/contacts/agenda/ContactsAgendaApplication.java
@@ -0,0 +1,12 @@
+package com.contacts.agenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactsAgendaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactsAgendaApplication.class, args);
+    }
+}
---
+++ src/test/java/com/contacts/agenda/ContactsAgendaApplicationTests.java
@@ -0,0 +1,12 @@
+package com.contacts.agenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsAgendaApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
+
